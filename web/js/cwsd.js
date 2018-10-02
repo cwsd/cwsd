433075d7046b002d96a546b8b2945453c03f0924
cwsd <ralaindimby@gmail.com>
2018-10-02 17:28:31 +0200

    dev page

--- web/js/cwsd.js
+++ web/js/cwsd.js
@@ -17,17 +17,43 @@ $(document).ready(function(){
     });
 
     $('#clickAcc').click(function() {
-        $('html,body').animate({scrollTop: $("#videoHomepage").offset().top}, 'slow'      );
+        $('html,body').animate({scrollTop: $("#videoHomepage").offset().top},'slow');
     });
 
     $('#clickPres').click(function() {
-        $('html,body').animate({scrollTop: $("#presH2").offset().top}, 'slow'      );
+        $('html,body').animate({scrollTop: $("#presH2").offset().top},'slow');
     });
 
 
     $('#clickSer').click(function() {
-        $('html,body').animate({scrollTop: $("#serh2").offset().top}, 'slow'      );
+        $('html,body').animate({scrollTop: $("#serh2").offset().top},'slow');
     });
+
+    $('#clickRea').click(function() {
+        $('html,body').animate({scrollTop: $("#reah2").offset().top},'slow');
+    });
+
+    $('#clickDis').click(function() {
+        $('html,body').animate({scrollTop: $("#dish2").offset().top},'slow');
+    });
+
+    $('#clickCon').click(function() {
+        $('html,body').animate({scrollTop: $("#conh2").offset().top},'slow');
+    });
+
+    $('#glyphiconDown2').click(function() {
+        $('html,body').animate({scrollTop: $("#conh2").offset().top},'slow');
+    });
+
+    $('#SlideH2Text').click(function() {
+        $('html,body').animate({scrollTop: $("#presH2").offset().top},'slow');
+    });
+
+    $('#glyphiconUp').click(function() {
+        $('html,body').animate({scrollTop: $("#videoHomepage").offset().top},'slow');
+    });
+
+
 
 
 
